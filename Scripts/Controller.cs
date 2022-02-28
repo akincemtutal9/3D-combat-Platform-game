8d12f31ccf58aece2174203602bdd6b4ce70bc8b
akincemtutal9 <akintutall@gmail.com>
2022-02-28 22:04:01 +0300

    Movement & Camera
    Movement & Camera Script

--- Scripts/Controller.cs
+++ Scripts/Controller.cs
@@ -4,15 +4,52 @@ using UnityEngine;
 
 public class Controller : MonoBehaviour
 {
-    // Start is called before the first frame update
+    float inputX;
+    float inputZ;
+
+    public Transform model;
+
+    Animator anim;
+
+    Vector3 moveDirection;
+
+    Camera mainCam;
+
+    public float damp;
+
+    public float rotationSpeed;
+
+
+
     void Start()
     {
-        
+        anim = GetComponent<Animator>();
+        mainCam = Camera.main;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        inputX = Input.GetAxis("Horizontal");
+        inputZ = Input.GetAxis("Vertical");
+
+        moveDirection = new Vector3(inputX, 0, inputZ);
+
+        InputMove();
+        InputRotation();
+    
+    
+    }
+    void InputMove()
+    {
+        anim.SetFloat("speed", Vector3.ClampMagnitude(moveDirection, 1).magnitude, damp, Time.deltaTime);
+
+    }
+    void InputRotation()
+    {
+        Vector3 rotOfSet = mainCam.transform.TransformDirection(moveDirection);
+        rotOfSet.y = 0;
+
+        model.forward = Vector3.Slerp(model.forward, rotOfSet, Time.deltaTime * rotationSpeed); 
     }
 }
