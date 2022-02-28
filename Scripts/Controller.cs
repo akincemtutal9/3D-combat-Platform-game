6c1c0e853286306cc2f08bf442720038d0ef2d38
akincemtutal9 <akintutall@gmail.com>
2022-02-28 23:12:10 +0300

    Update Controller.cs

--- Scripts/Controller.cs
+++ Scripts/Controller.cs
@@ -4,45 +4,80 @@ using UnityEngine;
 
 public class Controller : MonoBehaviour
 {
+    
+    public float damp;
+    public float rotationSpeed;
+    float normalFov;
+    public float sprintFov;
+
+    float maxSpeed;
     float inputX;
     float inputZ;
+
+
 
     public Transform model;
 
     Animator anim;
-
     Vector3 moveDirection;
-
     Camera mainCam;
 
-    public float damp;
+    public KeyCode SprintButton = KeyCode.LeftShift;
 
-    public float rotationSpeed;
-
+    public KeyCode walkButton = KeyCode.C;
 
 
     void Start()
     {
         anim = GetComponent<Animator>();
         mainCam = Camera.main;
+        normalFov = mainCam.fieldOfView;
+
     }
 
     // Update is called once per frame
-    void Update()
+    private void LateUpdate()
     {
-        inputX = Input.GetAxis("Horizontal");
-        inputZ = Input.GetAxis("Vertical");
-
-        moveDirection = new Vector3(inputX, 0, inputZ);
+      
 
         InputMove();
         InputRotation();
+        Movement();
     
     
     }
+    void Movement()
+    {
+        moveDirection = new Vector3(inputX, 0, inputZ);
+
+        if (Input.GetKey(SprintButton))
+        {
+            mainCam.fieldOfView = Mathf.Lerp(mainCam.fieldOfView, sprintFov, Time.deltaTime * 2);
+            
+            maxSpeed = 2f;
+            inputX = 2 * Input.GetAxis("Horizontal");
+            inputZ = 2 * Input.GetAxis("Vertical");
+        }
+        else if (Input.GetKey(walkButton))
+        {
+            mainCam.fieldOfView = Mathf.Lerp(mainCam.fieldOfView, normalFov, Time.deltaTime * 2);
+            maxSpeed = 0.2f;
+            inputX = Input.GetAxis("Horizontal");
+            inputZ = Input.GetAxis("Vertical");
+        }
+        else
+        {
+            mainCam.fieldOfView = Mathf.Lerp(mainCam.fieldOfView, normalFov, Time.deltaTime * 2);
+            maxSpeed = 1f;
+            inputX = Input.GetAxis("Horizontal");
+            inputZ = Input.GetAxis("Vertical");
+        }
+        
+    }
+    
     void InputMove()
     {
-        anim.SetFloat("speed", Vector3.ClampMagnitude(moveDirection, 1).magnitude, damp, Time.deltaTime);
+        anim.SetFloat("speed", Vector3.ClampMagnitude(moveDirection, maxSpeed).magnitude, damp, Time.deltaTime * 10);
 
     }
     void InputRotation()
