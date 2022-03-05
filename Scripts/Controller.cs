b4510381e3b06b6b061f1f4902beaee1cdbbd84c
akincemtutal9 <akintutall@gmail.com>
2022-03-05 23:49:09 +0300

    Strafe Movement
    Strafe Movement

--- Scripts/Controller.cs
+++ Scripts/Controller.cs
@@ -6,7 +6,11 @@ public class Controller : MonoBehaviour
 {
     
     public float damp;
+    [Range(1,20)]
     public float rotationSpeed;
+    [Range(1, 20)]
+    public float strafeTurnSpeed;
+
     float normalFov;
     public float sprintFov;
     bool isGrounded;
@@ -29,10 +33,22 @@ public class Controller : MonoBehaviour
 
 
     public KeyCode SprintButton = KeyCode.LeftShift;
-
     public KeyCode walkButton = KeyCode.C;
-
     public KeyCode jumpButton = KeyCode.Space;
+
+    public enum MovementType
+    {
+        Directional,
+        Strafe
+    };
+
+    public MovementType movementType;
+    
+    
+    
+    
+    
+    
     void Start()
     {
         anim = GetComponent<Animator>();
@@ -44,7 +60,7 @@ public class Controller : MonoBehaviour
     }
 
     // Update is called once per frame
-    private void LateUpdate()
+    private void Update()
     {
         isGrounded = Physics.CheckCapsule(cc.bounds.center, new Vector3(cc.bounds.center.x, cc.bounds.min.y - 0.1f, cc.bounds.center.z), 0.18f,groundMask);
         InputMove();
@@ -56,6 +72,57 @@ public class Controller : MonoBehaviour
     }
     void Movement()
     {
+        if (movementType == MovementType.Strafe)
+        {
+            anim.SetFloat("inputX", inputX, damp, Time.deltaTime * 10);
+            anim.SetFloat("inputZ", inputZ, damp, Time.deltaTime * 10);
+
+            // var
+            bool isMoving = inputX != 0 || inputZ != 0;
+            if (isMoving)
+            {
+                float yawCamera = mainCam.transform.rotation.eulerAngles.y;
+                transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(0, yawCamera, 0), strafeTurnSpeed * Time.fixedDeltaTime);
+            }
+
+
+
+
+
+
+
+
+        }
+        if(movementType == MovementType.Directional)
+        {
+            moveDirection = new Vector3(inputX, 0, inputZ);
+
+            if (Input.GetKey(SprintButton))
+            {
+                mainCam.fieldOfView = Mathf.Lerp(mainCam.fieldOfView, sprintFov, Time.deltaTime * 2);
+
+                maxSpeed = 2f;
+                inputX = 2 * Input.GetAxis("Horizontal");
+                inputZ = 2 * Input.GetAxis("Vertical");
+            }
+            else if (Input.GetKey(walkButton))
+            {
+                mainCam.fieldOfView = Mathf.Lerp(mainCam.fieldOfView, normalFov, Time.deltaTime * 2);
+                maxSpeed = 0.2f;
+                inputX = Input.GetAxis("Horizontal");
+                inputZ = Input.GetAxis("Vertical");
+            }
+            else
+            {
+                mainCam.fieldOfView = Mathf.Lerp(mainCam.fieldOfView, normalFov, Time.deltaTime * 2);
+                maxSpeed = 1f;
+                inputX = Input.GetAxis("Horizontal");
+                inputZ = Input.GetAxis("Vertical");
+            }
+        }
+        
+        
+        /*
         moveDirection = new Vector3(inputX, 0, inputZ);
 
         if (Input.GetKey(SprintButton))
@@ -80,7 +147,7 @@ public class Controller : MonoBehaviour
             inputX = Input.GetAxis("Horizontal");
             inputZ = Input.GetAxis("Vertical");
         }
-        
+        */
     }
     
     void InputMove()
@@ -92,7 +159,6 @@ public class Controller : MonoBehaviour
     {
         Vector3 rotOfSet = mainCam.transform.TransformDirection(moveDirection);
         rotOfSet.y = 0;
-
         model.forward = Vector3.Slerp(model.forward, rotOfSet, Time.deltaTime * rotationSpeed); 
     }
     
@@ -100,8 +166,7 @@ public class Controller : MonoBehaviour
     {
         if (Input.GetKey(jumpButton))
         {
-            rb.AddForce(Vector3.up * jump);
+            rb.AddForce(Vector3.up.normalized * jump);
         }
-
     }
 }
