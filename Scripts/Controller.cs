0598acac06f40daa7e284e1a6c2ed26179895460
akincemtutal9 <akintutall@gmail.com>
2022-03-07 15:11:20 +0300

    Character Movement
    WEEK3 Character Movement Basics

--- Scripts/Controller.cs
+++ Scripts/Controller.cs
@@ -5,37 +5,53 @@ using UnityEngine;
 public class Controller : MonoBehaviour
 {
 
+    //SetFLoattaki damp bekleme
     public float damp;
     [Range(1, 20)]
+    //Rotation speed basically
     public float rotationSpeed;
+    /*
     [Range(1, 20)]
     public float strafeTurnSpeed;
-
+    */
+    
+    //Kamera koþmazken normal giderken ki uzaklýk
     float normalFov;
+    //Koþarken ki Kamera Fovu
     public float sprintFov;
     
-    
+    // player grounded mý deðil mi BOOLEANI
     bool isGrounded;
     
-
+    // PLayer koþarken ki speed
     float maxSpeed;
+    // Playerýn jump speedi
     [SerializeField] float jump;
+    //Basic inputX
     float inputX;
+    //Basis inputZ
     float inputZ;
 
 
-
+    //Model için Transfrom 
     public Transform model;
 
+
+    // isGrounded check için groundMask
     [SerializeField] private LayerMask groundMask;
+    // Animator almak için
     Animator anim;
+    // Basic VECTOR3 haraket için
     Vector3 moveDirection;
+    //Kamerayla oynamak için
     Camera mainCam;
+    // RIGIDBODY
     Rigidbody rb;
+    //Collider
     CapsuleCollider cc;
     
 
-
+    //Buttons
     public KeyCode SprintButton = KeyCode.LeftShift;
     public KeyCode walkButton = KeyCode.C;
     public KeyCode jumpButton = KeyCode.Space;
@@ -47,7 +63,6 @@ public class Controller : MonoBehaviour
     };
 
     public MovementType movementType;
-
 
 
 
@@ -66,17 +81,18 @@ public class Controller : MonoBehaviour
     // Update is called once per frame
     private void Update()
     {
+        // Collider dan Physics sýnýfýndan checkCapsule(boolean) method kullanarak grounded mý check ediyoruz 
         isGrounded = Physics.CheckCapsule(cc.bounds.center, new Vector3(cc.bounds.center.x, cc.bounds.min.y - 0.1f, cc.bounds.center.z), 0.18f, groundMask);
         InputMove();
         InputRotation();
         Movement();
-        if (isGrounded&&Input.GetKeyDown(jumpButton)) {Jump();}
+        Jump();
 
 
     }
     void Movement()
     {
-        if (movementType == MovementType.Strafe)
+       /* if (movementType == MovementType.Strafe)
         {
             anim.SetFloat("inputX", inputX, damp, Time.deltaTime * 10);
             anim.SetFloat("inputZ", inputZ, damp, Time.deltaTime * 10);
@@ -89,7 +105,7 @@ public class Controller : MonoBehaviour
                 transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(0, yawCamera, 0), strafeTurnSpeed * Time.fixedDeltaTime);
             }
         }
-
+       */
 
 
 
@@ -97,8 +113,10 @@ public class Controller : MonoBehaviour
         {
             moveDirection = new Vector3(inputX, 0, inputZ);
 
+            //GetKey butona basýlý tutunca calýsmasý acýsýndan 
             if (Input.GetKey(SprintButton))
             {
+                //Lerp Kamerayý ileri ve geri haraket ettirecek
                 mainCam.fieldOfView = Mathf.Lerp(mainCam.fieldOfView, sprintFov, Time.deltaTime * 2);
 
                 maxSpeed = 2f;
@@ -151,28 +169,38 @@ public class Controller : MonoBehaviour
 
     void InputMove()
     {
-        anim.SetFloat("speed", Vector3.ClampMagnitude(moveDirection, maxSpeed).magnitude, damp, Time.deltaTime * 100);
+        //Hýz 1 olsun istiyoruz ona göre hýzý 0 ile 1 arasýna sabitliyoruz  
+        // Time delta yý 10 la çarpýyoruz çünkü haraket etmeyi kesince karakter biraz daha animasyon yemeye devam ediyor
+        anim.SetFloat("speed", Vector3.ClampMagnitude(moveDirection, maxSpeed).magnitude, damp, Time.deltaTime * 10);
 
     }
     void InputRotation()
     {
+        //Karakter basýlan tarafa dönsün diye WASD için
         Vector3 rotOfSet = mainCam.transform.TransformDirection(moveDirection);
         rotOfSet.y = 0;
+        //Karakterin önü dönsün istediðimiz yere diye 
+        //Slerp rotasyon fonksiyonu optimize dönüþ saglýyor
         model.forward = Vector3.Slerp(model.forward, rotOfSet, Time.deltaTime * rotationSpeed);
     }
 
     void Jump()
     {
-        
-        if (isGrounded == false)
+        // Ground Check yapýp karakteri zýplatýyoruz ya da zýplatmýyoruz 
+        // maksat bir kereden fazla zýplamasýný önlemek
+        if (isGrounded&&Input.GetKeyDown(jumpButton))
         {
+            rb.AddForce(Vector3.up.normalized * jump);
             
-            anim.SetBool("isJumping", true);
+        }
+        if (isGrounded)
+        {
+            anim.SetBool("isJumping", false);
         }
         else
         {
-            rb.AddForce(Vector3.up.normalized * jump);
-            anim.SetBool("isJumping", false);
+            
+            anim.SetBool("isJumping", true);
         }
           
     }
---
+++ WEEK3.md
@@ -0,0 +1,23 @@
+#**TO-DO**
+-Character Jump Animation
+-Character Fall Animation
+-Character Land Animation
+
+#**DOING**
+-Character Jump Animation
+-Character Fall Animation
+
+#**DONE**
+-Character Basic Movement On Capsule(WASD)
+-Character Basic Camera
+---
+-ADD model from (Mixamo)
+-Character Basic Movement On model
+-Advanced Camera from (Unity Standart Assets)
+-IK Look
+-Character Walk Animation
+-Character Run Animation
+---
+-ADD Ground Check with Capsule Collider
+-ADD LayerMask = "Ground"(TAG)
+-Character JUMP
